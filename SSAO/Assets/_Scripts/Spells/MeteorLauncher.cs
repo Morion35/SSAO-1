bda08e326d028b19f9c47473ee2f2b51e1c2e58d
Morion35 <35432451+Morion35@users.noreply.github.com>
2018-04-30 19:11:22 +0200

    LOL ça pue le revert

---
+++ SSAO/Assets/_Scripts/Spells/MeteorLauncher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorLauncher : MonoBehaviour {
+
+	
+	public float speed;
+	public GameObject ImpactEffect;
+	
+	private Rigidbody rb;
+	
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.forward * speed;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Destroy(gameObject);
+		if (other.CompareTag("enemy"))
+		{
+			GameObject clone = Instantiate(ImpactEffect, other.transform.position + new Vector3(0,1000f,0), other.transform.rotation);
+		}
+	}
+}
