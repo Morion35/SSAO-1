9256790d5647dc28ac85f7fe715fb1035d76f365
Azuryion <35432664+Azuryion@users.noreply.github.com>
2018-04-30 13:22:04 +0200

    Healer fini

---
+++ SSAO/Assets/_Scripts/Spells/Mover.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+	public float speed;
+	public GameObject ImpactEffect;
+	
+	private Rigidbody rb;
+	
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.forward * speed;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Destroy(gameObject);
+		if (other.CompareTag("enemy"))
+		{
+			GameObject clone = Instantiate(ImpactEffect, other.transform.position + new Vector3(0, 0.25f, 0),
+				other.transform.rotation);
+		}
+	}
+}
