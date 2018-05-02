f7a02bd09fa5e4fbad10d2a3942d820ef8302b61
Morion35 <35432451+Morion35@users.noreply.github.com>
2018-05-02 22:40:55 +0200

    u

--- SSAO/Assets/_Scripts/FirstPersonController.cs
+++ SSAO/Assets/_Scripts/FirstPersonController.cs
@@ -73,7 +73,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private void Awake()
         {
             m_CharacterController = GetComponent<CharacterController>();
-            m_Camera = Camera.main;
+            m_Camera = GetComponentInChildren<Camera>();
             m_OriginalCameraPosition = m_Camera.transform.localPosition;
             m_FovKick.Setup(m_Camera);
             m_HeadBob.Setup(m_Camera, m_StepInterval);
