ca602645c4b64299f6c62847409bc707ba24ace0
Morion35 <35432451+Morion35@users.noreply.github.com>
2018-05-22 22:01:34 +0200

    kk

--- SSAO/Assets/_Scripts/MainMenu.cs
+++ SSAO/Assets/_Scripts/MainMenu.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Policy;
 using System.Xml.Linq;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
