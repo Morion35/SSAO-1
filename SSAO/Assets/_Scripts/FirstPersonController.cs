e8ae4bcb3623bfe6017517941e642a4a5f605617
Morion35 <35432451+Morion35@users.noreply.github.com>
2018-05-22 21:14:28 +0200

    +assets and settings
    ajouté les assets des armes, changé les paramètres de build et de player pour tenter d'optimiser

--- SSAO/Assets/_Scripts/FirstPersonController.cs
+++ SSAO/Assets/_Scripts/FirstPersonController.cs
@@ -13,23 +13,23 @@ namespace UnityStandardAssets.Characters.FirstPerson
     [RequireComponent(typeof (AudioSource))]
     public class FirstPersonController : MonoBehaviour
     {
-        [SerializeField] private bool m_IsWalking;
-        [SerializeField] public float m_WalkSpeed;
-        [SerializeField] private float m_RunSpeed;
-        [SerializeField] [Range(0f, 1f)] private float m_RunstepLenghten;
-        [SerializeField] private float m_JumpSpeed;
-        [SerializeField] private float m_StickToGroundForce;
-        [SerializeField] private float m_GravityMultiplier;
-        [SerializeField] private MouseLook m_MouseLook;
-        [SerializeField] private bool m_UseFovKick;
-        [SerializeField] private FOVKick m_FovKick = new FOVKick();
-        [SerializeField] private bool m_UseHeadBob;
-        [SerializeField] private CurveControlledBob m_HeadBob = new CurveControlledBob();
-        [SerializeField] private LerpControlledBob m_JumpBob = new LerpControlledBob();
-        [SerializeField] private float m_StepInterval;
-        [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
-        [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
-        [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
+        private bool m_IsWalking;
+        public float m_WalkSpeed;
+        private float m_RunSpeed;
+        private float m_RunstepLenghten;
+        private float m_JumpSpeed;
+        private float m_StickToGroundForce;
+        private float m_GravityMultiplier;
+        private MouseLook m_MouseLook;
+        private bool m_UseFovKick;
+        private FOVKick m_FovKick = new FOVKick();
+        private bool m_UseHeadBob;
+        private CurveControlledBob m_HeadBob = new CurveControlledBob();
+        private LerpControlledBob m_JumpBob = new LerpControlledBob();
+        private float m_StepInterval;
+        private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
+        private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
+        private AudioClip m_LandSound;           // the sound played when character touches back on ground.
 
         private Camera m_Camera;
         private bool m_Jump;
