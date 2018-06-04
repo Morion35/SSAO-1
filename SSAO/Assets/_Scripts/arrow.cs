4f0e3f5f76c0f8d168624f160234caae623013b2
Morion35 <35432451+Morion35@users.noreply.github.com>
2018-06-04 15:45:25 +0200

    added enemy damage and assets

---
+++ SSAO/Assets/_Scripts/arrow.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour
+{
+
+	public float speed;
+	public GameObject ImpactEffect;
+
+	private Transform initial;
+	
+	private Rigidbody rb;
+	
+	private void Start()
+	{
+		initial = transform.parent;
+		transform.parent = null;
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.forward * speed;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+    
+		if (!other.CompareTag("Spell"))
+		{
+	        
+			if (other.CompareTag("porte"))
+			{
+				Destroy(gameObject);
+				other.GetComponent<porte>().HP -= 20;
+			}
+	        
+			if (other.CompareTag("enemy"))
+			{
+				Destroy(gameObject);
+				GameObject clone = Instantiate(ImpactEffect, other.transform.position + new Vector3(0,0.25f,0), other.transform.rotation);
+			}
+
+			if (other.CompareTag("Player"))
+			{
+				if (GetComponent<MeshFilter>() == null)
+				{
+					GameObject clone = Instantiate(ImpactEffect, other.transform.position + new Vector3(0,0.25f,0), other.transform.rotation);
+					Destroy(gameObject);
+				}
+			}
+			else
+			{
+				rb.velocity = new Vector3(0,0,0);
+				transform.SetPositionAndRotation(transform.position + (transform.forward/10), transform.rotation);
+			}
+		}
+	}
+}
--- SSAO/Assets/_Scripts/enemyMovement.cs
+++ SSAO/Assets/_Scripts/enemyMovement.cs
@@ -15,6 +15,8 @@ public class enemyMovement : MonoBehaviour {
 	public float HP = 100;
 	
 	private GameObject[] Players;
+
+	public GameObject player;
 	
 	AudioSource audio;
 
@@ -29,6 +31,12 @@ public class enemyMovement : MonoBehaviour {
     private Vector3 initialPos;
 
 	private Quaternion initialDir;
+
+	public float firetime = 3f;
+
+	public float damage = 25f;
+
+	private float fireuse;
 
 	// Use this for initialization
 	void Start () {
@@ -52,7 +60,7 @@ public class enemyMovement : MonoBehaviour {
         nav = GetComponent<NavMeshAgent>();
 
         anim = GetComponent<Animator>();
-        
+		fireuse = 0f;
 	}
 	
 	// Update is called once per frame
@@ -80,19 +88,20 @@ public class enemyMovement : MonoBehaviour {
 					    && !nav.Raycast(player.transform.position, out hit) || player.GetComponent<AudioSource>().minDistance > (player.transform.position - transform.position).magnitude)
 					{
 						Player = player.transform;
+						this.player = player;
 						audio = player.GetComponent<AudioSource>();
 						isFocused = true;
 						break;
 					}
 					if ((player.transform.position - transform.position).magnitude < (Player.position - transform.position).magnitude)
 					{
+						this.player = player;
 						Player = player.transform;
 						audio = player.GetComponent<AudioSource>();
 					}
 					
 				}
 			}
-			
 		}
 		Vector3 targetDir = Player.position - transform.position;
 		float angle = Vector3.Angle(targetDir, transform.forward);
@@ -110,7 +119,7 @@ public class enemyMovement : MonoBehaviour {
 		else
 		{
 			isFocused = false;
-			if (anim.GetBool("detected") && (Player.position - transform.position).magnitude < 2f)
+			if (anim.GetBool("detected") && (Player.position - transform.position).magnitude < 1f)
 			{
 				_time = 0f;
 				nav.isStopped = false;
@@ -120,44 +129,52 @@ public class enemyMovement : MonoBehaviour {
 				nav.SetDestination(Player.position);
 				isFocused = true;
 			}
-			
-			if (audio.maxDistance > targetDir.magnitude)
+			else
 			{
-				anim.SetBool("suspicious",true);
-				_fov = 90f;
-			}
-			if ((nav.destination - transform.position).magnitude <= 0.2f)
-			{
-				anim.SetBool("detected", false);
-				if (anim.GetBool("walking"))
-				{
-					if (_time > 3f)
-					{
-						anim.SetBool("suspicious", false);
-						_fov = 60f;
-						_time = 0f;
-					}
-					anim.SetBool("walking",false);
-					nav.isStopped = true;
-					transform.rotation = initialDir;
-				}
-				else
+				if (audio.maxDistance > targetDir.magnitude)
 				{
 					anim.SetBool("suspicious",true);
-					if (_time > 3f)
-					{
-						anim.SetBool("suspicious", false);
-						anim.SetBool("walking", true);
-						nav.SetDestination(initialPos);
-						_time = 0f;
-					}
+					_fov = 90f;
 				}
-				if (anim.GetBool("suspicious"))
+				if ((nav.destination - transform.position).magnitude <= 0.2f)
 				{
-					_time += Time.deltaTime;
-				}
+					anim.SetBool("detected", false);
+					if (anim.GetBool("walking"))
+					{
+						if (_time > 3f)
+						{
+							anim.SetBool("suspicious", false);
+							_fov = 60f;
+							_time = 0f;
+						}
+						anim.SetBool("walking",false);
+						nav.isStopped = true;
+						transform.rotation = initialDir;
+					}
+					else
+					{
+						anim.SetBool("suspicious",true);
+						if (_time > 3f)
+						{
+							anim.SetBool("suspicious", false);
+							anim.SetBool("walking", true);
+							nav.SetDestination(initialPos);
+							_time = 0f;
+						}
+					}
+					if (anim.GetBool("suspicious"))
+					{
+						_time += Time.deltaTime;
+					}
+			}
+			
 			}
 		}
-		
+		if (anim.GetBool("detected") && (Player.position - transform.position).magnitude < 0.45f && Time.time > fireuse)
+		{
+			fireuse = Time.time + firetime;
+			player.GetComponent<PlayerStatus>().HP -=
+				damage - (damage * player.GetComponent<PlayerStatus>().armor / 100);
+		}
     }
 }
