6e666e6b7a250e0194b39339bff851c7eca9ec8f
Azuryion <35432664+Azuryion@users.noreply.github.com>
2018-06-07 11:51:09 +0200

    kk

--- SSAO/Assets/_Scripts/animate.cs
+++ SSAO/Assets/_Scripts/animate.cs
@@ -1,27 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 
-public class animate : NetworkBehaviour
+public class animate : MonoBehaviour
 {
 	private Animator anim;
-
-	private NetworkAnimator netanim;
 	// Use this for initialization
 	void Awake ()
 	{
-			anim = GetComponent<Animator>();
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (!isLocalPlayer)
-			return;
-		
 		float h = Input.GetAxisRaw("Horizontal");
 		float v = Input.GetAxisRaw("Vertical");
-		anim.SetBool("iswalking", h != 0f || v != 0f);
+		anim.SetBool("iswalking", h!=0f || v != 0f);
 	}
 }
