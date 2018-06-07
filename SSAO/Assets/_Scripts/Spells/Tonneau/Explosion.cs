0f76b2f6165b493f1f16549cdf5f01f0eea0c2da
Azuryion <35432664+Azuryion@users.noreply.github.com>
2018-06-07 21:59:41 +0200

    maj des tonneaux

---
+++ SSAO/Assets/_Scripts/Spells/Tonneau/Explosion.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour
+{
+    private float damage = 40f;
+
+    private GameObject[] players;
+    private GameObject[] enemies;
+
+    private void Awake()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        enemies = GameObject.FindGameObjectsWithTag("enemy");
+
+        foreach (GameObject enemy in enemies)
+        {
+            if (enemy != null && (enemy.transform.position - transform.position).magnitude <= 2f)
+            {
+                enemy.GetComponent<enemyMovement>().HP -= damage;
+            }
+        }
+        foreach (GameObject player in players)
+        {
+            if ((player.transform.position - transform.position).magnitude <= 2f)
+            {
+                player.GetComponent<PlayerStatus>().HP -= (damage - (damage*player.GetComponent<PlayerStatus>().armor/100));
+            }
+        }
+    }
+    
+    
+}
